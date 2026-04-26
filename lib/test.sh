6f3196579ba1a3f21a9629950b9492e87cb6b978
Ludovic Agathe <ludovic.agathe@gmail.com>
2026-04-26 16:43:26 +0400

    202604261643 - Unblocked testing for edit_line. Left to test for '/' or '|' separators present in regex pattern

--- lib/test.sh
+++ lib/test.sh
@@ -1,19 +1,14 @@
-LOGGING_STORE=""
-
 assert() { # Test function requiring 3 arguments: 1. a string describing the test; 2. the expected result string or number; 3. the actual outcome of a function or other output from $(). Prints expected and result on failure, clipping both to their first 20 characters.
+## Todo: make assert accept several possible results
   local DESCRIPTION=$1
   local EXPECTED=$2
   local RESULT=$3
   local CHAR_LIMIT=50
   if [[ ${#} -ne 3 ]]; then
     echo -e "\033[1;31mThe number of arguments provided to the assert function should be 3\033[0m" >&2
-    echo "$1"
-    echo "$2"
-    echo "$3"
-    echo "$4"
     return 1
   fi
-  if [[ "$RESULT" != $EXPECTED ]]; then
+  if [[ "$RESULT" != "$EXPECTED" ]]; then
     if [[ "${#RESULT}" -gt "$CHAR_LIMIT" ]]; then
       RESULT="${RESULT:0:$CHAR_LIMIT} (...)"
     fi
@@ -28,12 +23,8 @@ assert() { # Test function requiring 3 arguments: 1. a string describing the tes
   echo -e "\033[1;32mTEST: \033[0m${DESCRIPTION}: \033[1;32mPassed\033[0m"
 }
 
-run_assert() {
-  echo "$#"
-  echo "$1"
-  echo "$2"
-  echo "$3"
-  echo -e "\033[1;33mTEST:\033[0m $1" >&2
-  assert "$@"
-  LOGGING_STORE="${LOGGING_STORE}\n$(assert $@)"
+run_assert() { # Wrapper around the assert function, providing pre-test description for user input indication and/or instruction and providing a full test summary, stored in the LOGGING_STORE global variable. The LOGGING_STORE variable MUST be provided in the calling script.
+  echo -e "\033[1;33m## TEST:\033[0m $1" >&2
+  local RESULT=$(assert "$@")
+  LOGGING_STORE="${LOGGING_STORE}\n${RESULT}"
 }
--- tests/unit_tests/test_edit_line.sh
+++ tests/unit_tests/test_edit_line.sh
@@ -1,22 +1,30 @@
+# This test script runs tests on the edit_line function. LOGGING_STORE variable is preserved and restored at the end of the tests. 
+## REQUIREMENTS:
+### 1. The LOGGING_STORE variable is to be provided
+### 2. LOGGING_STORE to be echoed to summarise test results
 source ../lib/test.sh
 ERROR_STRING="Err"
+if [[ -n "$LOGGING_STORE" ]]; then
+  OLD_LOGGING_STORE="${LOGGING_STORE}"
+fi
 LOGGING_STORE=""
 
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'No argument passed' $ERROR_STRING $(edit_line))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert '1 argument passed' $ERROR_STRING $(edit_line 'test'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert '2 argument passed' $ERROR_STRING $(edit_line 'test' 'test2'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert '3rd argument is not a regular file' $ERROR_STRING $(edit_line 'test' 'test2' 'alpha'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search string not in file' 0 $(edit_line 'test' 'test2' 'sc_test'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search string in file' 4 $(edit_line 'line' 'test' 'sc_test'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search for regex pattern' 1 $(edit_line 's.*me' 'test' 'sc_test'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search for basic regex pattern' 1 $(edit_line 'c\(o\+d\?\)e' 'test' 'sc_test'))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search for basic regex pattern multiple replacements (input 'y' twice)' 2 $(edit_line 'c\(o\?d\?\)e' 'test' 'sc_test' -v))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search for basic regex pattern multiple replacements (input 'n' once)' 1 $(edit_line 'c\(o\?d\?\)e' 'test' 'sc_test' -v))"
-# LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search for basic regex pattern multiple replacements (input 'n' twice)' 0 $(edit_line 'c\(o\?d\?\)e' 'test' 'sc_test' -v))"
-# # LOGGING_STORE="${LOGGING_STORE}\n$(assert 'Search for basic regex pattern single replacement mode (input 'n' once)' 1 $(edit_line 'l\(i\?n\?\)e' 'test' 'sc_test' -s))"
-run_assert "Search for basic regex pattern single replacement mode (input 'n' once first)" 1 "$(edit_line 'l\(i\?n\?\)e' 'test' 'sc_test' -s)"
+run_assert 'No argument passed' $ERROR_STRING "$(edit_line)"
+run_assert '1 argument passed' $ERROR_STRING "$(edit_line 'test')"
+run_assert '2 argument passed' $ERROR_STRING "$(edit_line 'test' 'test2')"
+run_assert '3rd argument is not a regular file' $ERROR_STRING "$(edit_line 'test' 'test2' 'alpha')"
+run_assert 'Search string not in file' 0 "$(edit_line 'test' 'test2' 'sc_test')"
+run_assert 'Search string in file' 2 "$(edit_line 'line' 'test' 'sc_test')"
+run_assert 'Search for regex pattern' 1 "$(edit_line 's.*me' 'test' 'sc_test')"
+run_assert 'Search for basic regex pattern' 1 "$(edit_line 'c\(o\+d\?\)e' 'test' 'sc_test')"
+run_assert 'Search for basic regex pattern multiple replacements (input 'y' twice)' 2 "$(edit_line 'c\(o\?d\?\)e' 'test' 'sc_test' -v)"
+run_assert 'Search for basic regex pattern multiple replacements (input 'n' once)' 1 "$(edit_line 'c\(o\?d\?\)e' 'test' 'sc_test' -v)"
+run_assert 'Search for basic regex pattern no replacement (input 'n' twice)' 0 "$(edit_line 'c\(o\?d\?\)e' 'test' 'sc_test' -v)"
+run_assert 'Search for basic regex pattern single replacement mode (input 'n' once)' 1 "$(edit_line 'li\(n\?\)e' 'test' 'sc_test' -s)"
+run_assert "Search for basic regex pattern single replacement mode (input 'n' once first)" 1 "$(edit_line 'li\(n\?\)e' 'test' 'sc_test' -s)"
 
 echo
 echo -e "${YELLOW}     *****     RESULTS     *****${NC}"
 echo -e "${LOGGING_STORE}"
 # check for basic regex
+LOGGING_STORE="${OLD_LOGGING_STORE}"
